b7725eae5db827ad5d97b9bfdea866ae2dab58f7
Iftekhar-007 <shawonsp534@gmail.com>
2026-03-01 11:42:43 +0600

    server running

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
-import express, { Application, Request, Response } from "express";
+import app from "./app";
 
-const app: Application = express();
 const port = 5000; // The port your express server will be running on.
-
-// Enable URL-encoded form data parsing
-app.use(express.urlencoded({ extended: true }));
-
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// Basic route
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript + Express!");
-});
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+const bootstrap = () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server", error);
+  }
+};
+
+bootstrap();
