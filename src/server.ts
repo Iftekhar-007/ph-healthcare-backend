98faed5933155d374b050c0c294e061e7b87315e
Iftekhar-007 <shawonsp534@gmail.com>
2026-03-01 12:35:33 +0600

    prisma init

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,6 @@
 import app from "./app";
 
-const port = 5000; // The port your express server will be running on.
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+const port = 5000;
 
 const bootstrap = () => {
   try {
