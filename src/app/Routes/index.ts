e3ef14ccd9921c020c7a85b601ec2a970b8aa2b2
Iftekhar-007 <shawonsp534@gmail.com>
2026-03-07 14:06:19 +0600

    doctor create route done

--- src/app/Routes/index.ts
+++ src/app/Routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { SpecialtyRoutes } from "../module/specialty/specialty.route";
 import { AuthRoutes } from "../module/auth/auth.route";
+import { userRouter } from "../module/user/user.route";
 
 const router = Router();
 
 router.use("/auth", AuthRoutes);
 
 router.use("/specialties", SpecialtyRoutes);
+
+router.use("/doctors", userRouter);
 
 export const IndexRoutes = router;
